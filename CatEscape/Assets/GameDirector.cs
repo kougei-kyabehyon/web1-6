4a70550747407c40f1b1d9fdcb9a48016d366a17
Unknown <sorldart0313@i.softbank.jp>
2018-06-04 18:30:59 +0900

    0604

---
+++ CatEscape/Assets/GameDirector.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameDirector : MonoBehaviour {
+
+
+
+    GameObject hoGauge;
+	// Use this for initialization
+	void Start () {
+        this.hoGauge = GameObject.Find("hpGauge");
+	}
+	
+	// Update is called once per frame
+	public void DecreaseHp () {
+        this.hoGauge.GetComponent<Image>().fillAmount -= 0.1f;
+	}
+}
---
+++ CatEscape/Assets/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+    public void LButtonDown()
+    {
+        //左矢印が押されたとき
+        
+            transform.Translate(-3, 0, 0);
+        
+    }
+    public void RButtonDown()
+    {
+       
+            transform.Translate(3, 0, 0);
+
+        
+    }
+	
+}
